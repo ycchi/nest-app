5432182ed8c7db08a74779f6679c136e690b5c98
Young CHi <young.c.chi@gmail.com>
2021-01-13 15:51:03 -0500

    working on guard

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+// import { JwtAuthGuard } from './authentication/google/jwt-auth-google.guard';
 
 @Controller('')
 export class AppController {
   @Get()
   home() {
     return 'Welcome to Movie API';
+  }
+
+  @Get('test')
+  @UseGuards(AuthGuard('google'))
+  test() {
+    return 'auth successful';
   }
 }
