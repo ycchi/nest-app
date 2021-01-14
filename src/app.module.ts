06afdab1c8c9b2006e6dd23b2acb1bb808199603
Young CHi <young.c.chi@gmail.com>
2021-01-13 21:46:36 -0500

    wip

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MoviesModule } from './movies/movies.module';
 import { AppController } from './app.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MoviesModule],
+  imports: [MoviesModule, AuthModule],
   controllers: [AppController],
   providers: [],
 })
